3709357ae9a1d5b724b09b3b0cd734e9075297d4
Corporación 2001 <corporacion2001.dev@gmail.com>
2025-11-17 15:05:31 -0600

    Update Step3Service.jsx

--- taller-frontend/src/components/serviceManagement/serviceStepper/step3Service/Step3Service.jsx
+++ taller-frontend/src/components/serviceManagement/serviceStepper/step3Service/Step3Service.jsx
@@ -296,7 +296,7 @@ const Step3Service = ({
         throw new Error("Por favor corrija los errores en el formulario");
       }
 
-           if (photos.length < 0) {
+           if (photos.length < 10) {
         throw new Error("Debe subir al menos 10 fotos del vehículo");
       }
 
